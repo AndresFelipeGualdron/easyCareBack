6d0db6316b737ae35f60d16474b64948f7985884
nattpalacios <nataplc24@gmail.com>
2020-03-12 10:00:21 -0500

    Arreglando tablas

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -13,7 +13,8 @@ documento VARCHAR(50) NOT NULL,
 tipoDocumento VARCHAR(50) NOT NULL,
 nombre VARCHAR(100) NOT NULL,
 correo VARCHAR(50) NOT NULL,
-telefono VARCHAR(70) NOT NULL
+telefono VARCHAR(70) NOT NULL,
+idPaseoEnCurso INTEGER
 );
 
 CREATE TABLE Mascota(
@@ -21,21 +22,70 @@ id INTEGER NOT NULL,
 nombre VARCHAR(50) NOT NULL,
 raza VARCHAR(50) NOT NULL,
 edad INTEGER NOT NULL,
-genero VARCHAR(50) NOT NULL
+genero VARCHAR(50) NOT NULL,
+idCliente VARCHAR(50) NOT NULL,
+idPaseador VARCHAR(50),
+idPaseoEnCurso INTEGER
 );
 
 CREATE TABLE Paseo(
 id INTEGER NOT NULL,
-ruta VARCHAR(200) NOT NULL,
-duracion INTEGER NOT NULL
+duracion INTEGER,
+precio INTEGER NOT NULL,
+especificaciones VARCHAR(300),
+idRuta INTEGER NOT NULL
 );
 
 CREATE TABLE Subasta(
 id INTEGER NOT NULL,
-oferta INTEGER NOT NULL
+oferta INTEGER NOT NULL,
+creador VARCHAR(50) NOT NULL,
+idPaseo INTEGER NOT NULL
 );
 
+CREATE TABLE Ruta(
+id INTEGER NOT NULL,
+puntoPartida VARCHAR(50) NOT NULL,
+puntoLlegada VARCHAR(50) NOT NULL
+);
 
+CREATE TABLE PaseoEnCurso(
+id INTEGER NOT NULL,
+idPaseador VARCHAR(50) NOT NULL,
+idPaseo INTEGER NOT NULL,
+latitudUbicacion INTEGER NOT NULL,
+longitudUbicacion INTEGER NOT NULL
+);
+
+CREATE TABLE Ubicacion(
+latitud INTEGER NOT NULL,
+longitus INTEGER NOT NULL
+);
+
+CREATE TABLE Cliente_Paseo(
+idCliente VARCHAR(50) NOT NULL,
+idPaseo INTEGER NOT NULL
+);
+
+CREATE TABLE Paseador_Subasta(
+idPaseador VARCHAR(50) NOT NULL,
+idSubasta INTEGER NOT NULL
+);
+
+CREATE TABLE Paseador_Paseo(
+idPaseador VARCHAR(50) NOT NULL,
+idPaseo INTEGER NOT NULL
+);
+
+CREATE TABLE Cliente_Ruta(
+idCliente VARCHAR(50) NOT NULL,
+idRuta INTEGER NOT NULL
+);
+
+CREATE TABLE Cliente_Subasta(
+idCliente VARCHAR(50) NOT NULL,
+idSubasta INTEGER NOT NULL
+);
 
 --PRIMARIAS
 
@@ -43,3 +93,5 @@ oferta INTEGER NOT NULL
 CREATE SEQUENCE id_mascota;
 CREATE SEQUENCE id_paseo;
 CREATE SEQUENCE id_subasta;
+CREATE SEQUENCE id_ruta;
+CREATE SEQUENCE id_paseoEnCurso;
