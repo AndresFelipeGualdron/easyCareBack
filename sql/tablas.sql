b9770accb783beb138c06e7961c608af38cb2ebb
Natalia Palacios <nataplc24@gmail.com>
2020-03-16 18:21:30 -0500

    Arreglando sql

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -5,7 +5,8 @@ documento VARCHAR(50) NOT NULL,
 tipoDocumento VARCHAR(50) NOT NULL,
 nombre VARCHAR(100) NOT NULL,
 correo VARCHAR(50) NOT NULL,
-telefono VARCHAR(70) NOT NULL
+telefono VARCHAR(70) NOT NULL,
+password VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Paseador(
@@ -14,6 +15,7 @@ tipoDocumento VARCHAR(50) NOT NULL,
 nombre VARCHAR(100) NOT NULL,
 correo VARCHAR(50) NOT NULL,
 telefono VARCHAR(70) NOT NULL,
+password VARCHAR(200) NOT NULL,
 idPaseoEnCurso INTEGER
 );
 
@@ -59,7 +61,7 @@ longitudUbicacion INTEGER NOT NULL
 
 CREATE TABLE Ubicacion(
 latitud INTEGER NOT NULL,
-longitus INTEGER NOT NULL
+longitud INTEGER NOT NULL
 );
 
 CREATE TABLE Cliente_Paseo(
@@ -88,6 +90,145 @@ idSubasta INTEGER NOT NULL
 );
 
 --PRIMARIAS
+
+ALTER TABLE Cliente 
+   ADD CONSTRAINT PK_Cliente PRIMARY KEY (documento, tipoDocumento);
+
+ALTER TABLE Paseador 
+   ADD CONSTRAINT PK_Paseador PRIMARY KEY (documento, tipoDocumento);
+   
+ALTER TABLE Mascota 
+   ADD CONSTRAINT PK_Mascota PRIMARY KEY (id);
+
+ALTER TABLE Paseo 
+   ADD CONSTRAINT PK_Paseo PRIMARY KEY (id);
+   
+ALTER TABLE Subasta 
+   ADD CONSTRAINT PK_Subasta PRIMARY KEY (id);
+   
+ALTER TABLE Ruta 
+   ADD CONSTRAINT PK_Ruta PRIMARY KEY (id);
+   
+ALTER TABLE Ubicacion
+   ADD CONSTRAINT PK_Ubicacion PRIMARY KEY (latitud, longitud);
+   
+ALTER TABLE PaseoEnCurso 
+   ADD CONSTRAINT PK_PaseoEnCurso PRIMARY KEY (id);
+
+ALTER TABLE Cliente_Paseo 
+   ADD CONSTRAINT PK_Cliente_Paseo PRIMARY KEY (idCliente, idPaseo);
+
+ALTER TABLE Paseador_Subasta 
+   ADD CONSTRAINT PK_Paseador_Subasta PRIMARY KEY (idPaseador, idSubasta);
+
+ALTER TABLE Cliente_Ruta 
+   ADD CONSTRAINT PK_Cliente_Ruta PRIMARY KEY (idCliente, idRuta);
+
+ALTER TABLE Cliente_Subasta 
+   ADD CONSTRAINT PK_Cliente_Subasta PRIMARY KEY (idCliente, idSubasta);
+   
+ALTER TABLE Paseador_Paseo 
+   ADD CONSTRAINT PK_Paseador_Paseo PRIMARY KEY (idPaseador, idPaseo);
+   
+--FORANEAS
+
+ALTER TABLE Paseador
+ADD CONSTRAINT fk_paseoencurso
+    FOREIGN KEY (idPaseoEnCurso)
+    REFERENCES PaseoEnCurso (id);
+
+ALTER TABLE Mascota
+ADD CONSTRAINT fk_cliente
+    FOREIGN KEY (idCliente)
+    REFERENCES Cliente (documento, tipoDocumento);
+
+ALTER TABLE Mascota
+ADD CONSTRAINT fk_paseador
+    FOREIGN KEY (idPaseador)
+    REFERENCES Paseador (documento, tipoDocumento);
+
+ALTER TABLE Mascota
+ADD CONSTRAINT fk_paseo
+    FOREIGN KEY (idPaseoEnCurso)
+    REFERENCES PaseoEnCurso (id);
+	
+ALTER TABLE Paseo
+ADD CONSTRAINT fk_ruta
+    FOREIGN KEY (idRuta)
+    REFERENCES Ruta (id);
+	
+ALTER TABLE Subasta
+ADD CONSTRAINT fk_paseo
+    FOREIGN KEY (idPaseo)
+    REFERENCES Paseo (id);
+	
+ALTER TABLE PaseoEnCurso
+ADD CONSTRAINT fk_paseador
+    FOREIGN KEY (idPaseador)
+    REFERENCES Paseador (documento, tipoDocumento);
+
+ALTER TABLE PaseoEnCurso
+ADD CONSTRAINT fk_paseo
+    FOREIGN KEY (idPaseo)
+    REFERENCES Paseo (id);
+
+ALTER TABLE Cliente_Paseo
+ADD CONSTRAINT fk_cliente
+    FOREIGN KEY (idCliente)
+    REFERENCES Cliente (documento, tipoDocumento);
+	
+ALTER TABLE Cliente_Paseo
+ADD CONSTRAINT fk_paseo
+    FOREIGN KEY (idPaseo)
+    REFERENCES Paseo (id);
+	
+ALTER TABLE Paseador_Subasta
+ADD CONSTRAINT fk_paseador
+    FOREIGN KEY (idPaseador)
+    REFERENCES Paseador (documento, tipoDocumento);
+	
+ALTER TABLE Paseador_Subasta
+ADD CONSTRAINT fk_subasta
+    FOREIGN KEY (idSubasta)
+    REFERENCES Subasta (id);
+	
+ALTER TABLE Paseador_Paseo
+ADD CONSTRAINT fk_paseador
+    FOREIGN KEY (idPaseador)
+    REFERENCES Paseador (documento, tipoDocumento);
+	
+ALTER TABLE Paseador_Paseo
+ADD CONSTRAINT fk_paseo
+    FOREIGN KEY (idPaseo)
+    REFERENCES Paseo (id);
+	
+ALTER TABLE Cliente_Ruta
+ADD CONSTRAINT fk_cliente
+    FOREIGN KEY (idCliente)
+    REFERENCES Cliente (documento, tipoDocumento);
+	
+ALTER TABLE Cliente_Ruta
+ADD CONSTRAINT fk_ruta
+    FOREIGN KEY (idRuta)
+    REFERENCES Ruta (id);
+	
+ALTER TABLE Cliente_Subasta
+ADD CONSTRAINT fk_cliente
+    FOREIGN KEY (idCliente)
+    REFERENCES Cliente (documento, tipoDocumento);
+	
+ALTER TABLE Cliente_Subasta
+ADD CONSTRAINT fk_subasta
+    FOREIGN KEY (idSubasta)
+    REFERENCES Subasta (id);
+   
+--CHECKS
+
+ALTER TABLE Cliente ADD CONSTRAINT Correo
+		CHECK (Cliente.correo LIKE '%@%.%');
+		
+ALTER TABLE Paseador ADD CONSTRAINT Correo
+		CHECK (Paseador.correo LIKE '%@%.%');
 
 --SECUENCIAS
 CREATE SEQUENCE id_mascota;
